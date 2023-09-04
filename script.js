b2b643057b3cfd1da8249eba5b3d76991641fd73
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-04 13:40:41 -0400

    Add stekching feature

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("container");
-const cell = document.createElement("div");
-cell.classList = "cell";
+//const cell = document.createElement("div");
+//cell.classList = "cell";
 const createGridButton = document.getElementById("createGridButton");
 
 const createGrid = (length) => {
@@ -9,31 +9,24 @@ const createGrid = (length) => {
         for (let rows = 0; rows < length; rows++) {
             for (let columns = 0; columns < length; columns++) {
                 const cell = document.createElement("div");
-                console.log(cell);
                 container.appendChild(cell);
                 cell.classList = "cell";
-                let basis = 
                 cell.style.width = `${((1 / length) * 100)}%`;
                 cell.style.height = `${((1 / length) * 100)}%`
+                cell.addEventListener("mouseover", () => {
+                    cell.style.backgroundColor = "black";
+                })
             }
         }
     }
     else {
         alert("Error: Please enter valid number 1-100");
     }
+    
 }
-
-cell.addEventListener("mouseover", () => {
-cell.classList.add("highlighted");
-})
-
-cell.addEventListener("mouseout", () => {
-    cell.classList.remove("highlighted");
-})
 
 createGridButton.addEventListener("click", () => {
     let length = prompt("How many squares would you like per side? (Max 100)");
     createGrid(length);
 })
-
 
